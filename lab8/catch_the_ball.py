0619eee17c3545da563f87a9c09ad319f5f1ef7b
Your Name <you@example.com>
2022-10-31 12:19:38 +0300

    asdfsda

--- lab8/catch_the_ball.py
+++ lab8/catch_the_ball.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 2
+FPS = 60
 screen = pygame.display.set_mode((1200, 900))
 
 RED = (255, 0, 0)
@@ -22,6 +22,7 @@ def new_ball():
     r = randint(10, 100)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
+    return(x, y, r)
 
 def click(event):
     '''
@@ -33,15 +34,33 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+points = 0
+counter = 0
 while not finished:
     clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-    new_ball()
-    pygame.display.update()
-    screen.fill(BLACK)
 
+    if counter == 180:
+        screen.fill(BLACK)
+        x0, y0, r = new_ball()
+        pygame.display.update()
+        counter = 0
+        print(x0, y0, r)
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            print(x, y, x0, y0, r)
+            if (x - x0) ** 2 + (y - y0) ** 2 <= r ** 2:
+                print('+1')
+                points += 1
+    if event.type == pygame.QUIT:
+        finished = True
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        print('Click!')
+
+
+
+    counter += 1
+
+print(points)
 pygame.quit()
